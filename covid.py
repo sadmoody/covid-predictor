6dda1cf8fcef4087006a29e220f2305bbec84b12
Mahmood Hikmet <mahmood@hikmet.co.nz>
2020-03-26 23:14:26 +1300

    I'm semi-ok with how this is working but DiamondMinah might be right.

--- covid.py
+++ covid.py
@@ -6,8 +6,6 @@ import os
 from glob import glob
 import datetime
 
-DATADIR = "COVID-19\csse_covid_19_data\csse_covid_19_daily_reports"
-
 def func(x, a, b, c):
     return a * np.exp(-b * x) + c
 
@@ -17,24 +15,42 @@ def func_poly_four(x, a, b, c, d, e):
 def func_poly(x, a, b, c, d):
     return a * np.power(x, 3) + b * np.power(x, 2) + c * x + d
 
+START_DATE = datetime.datetime.now().date() - datetime.timedelta(days=16)
+END_DATE = START_DATE + datetime.timedelta(days=14)
+
 df = pd.read_json('data.min.json')
-df_us = df[df['Country'] == 'US'].tail(14)
-print(df_us)
-df_us['y'] = (df_us['Date'] - df_us['Date'].iloc[0]).dt.days
-
-ydata = df_us['Confirmed'].to_numpy()
-
-xdata = df_us['y'].to_numpy()
-print(xdata)
-print()
-print(df_us['y'])
-
-plt.plot(xdata, ydata, 'b-', label='data')
-popt, pcov = curve_fit(func_poly, xdata, ydata)
-plt.plot(np.linspace(-6,21), func_poly(np.linspace(-6,21), *popt), 'r-', label='fit: a=%5.3f, b=%5.3f, c=%5.3f, d=%5.3f' % tuple(popt))
-print(df_us)
-print(popt)
-print(f"next value is: {func_poly(df_us.iloc[df_us.shape[0]-1]['y']+1, *popt)}")
-print(f"next value is: {func_poly(df_us.iloc[df_us.shape[0]-1]['y']+2, *popt)}")
-print(f"next value is: {func_poly(df_us.iloc[df_us.shape[0]-1]['y']+3, *popt)}")
-plt.show()
+df.index = df['Date']
+df.drop('Date', axis=1, inplace=True)
+df['x'] = (df.index - pd.to_datetime(START_DATE)).days
+#filtered_df = df.loc[START_DATE:END_DATE]
+date_range = pd.date_range(START_DATE, END_DATE) 
+filtered_df = df[df.index.isin(date_range)]
+grouped = filtered_df.groupby('Country')
+formulas = {}
+for name, group in grouped: # do stuff.
+    df_us = group
+    ydata = df_us['Confirmed'].to_numpy()
+    xdata = df_us['x'].to_numpy()
+    popt, pcov = curve_fit(func_poly, xdata, ydata)
+    formulas[name] = {'confirmed':ydata, 'x':xdata, 'popt': popt, 'err': np.sqrt(np.diag(pcov))}
+    # print(f"{name}: {np.sqrt(np.diag(pcov))}")
+    # plt.plot(xdata, ydata, 'b-', label='data')
+    # plt.plot(np.linspace(-6,21), func_poly(np.linspace(-6,21), *popt), 'r-', label='fit: a=%5.3f, b=%5.3f, c=%5.3f, d=%5.3f' % tuple(popt))
+    # print(popt)
+    # print(f"today's value is: {df_us.iloc[df_us.shape[0]-1]['Confirmed']}")
+    # print(f"next value is: {func_poly(df_us.iloc[df_us.shape[0]-1]['x']+1, *popt)}")
+    # print(f"next value is: {func_poly(df_us.iloc[df_us.shape[0]-1]['x']+2, *popt)}")
+    # print(f"next value is: {func_poly(df_us.iloc[df_us.shape[0]-1]['x']+3, *popt)}")
+    # print(f"next value is: {func_poly(df_us.iloc[df_us.shape[0]-1]['x']+4, *popt)}")
+    # plt.show()
+for country in formulas:
+    # if country == "US":
+    current = formulas[country]
+    today_x = 14
+    print(f"COUNTRY: {country}\nerror:{abs(1.0-((func_poly(today_x, *(current['popt'])))/(current['confirmed'][-1])))}")
+    # print(f"yesterday's value: {current['confirmed'][-1]}")
+    # print(f"today's estimate: {func_poly(today_x, *(current['popt']))}")
+    # plt.plot(np.linspace(-6,21), func_poly(np.linspace(-6,21), *(current['popt'])), 'r-', label='fit: a=%5.3f, b=%5.3f, c=%5.3f, d=%5.3f' % tuple(popt))
+    # plt.plot(current['x'], current['confirmed'], 'b-', label='data')
+    # plt.show()
+        
