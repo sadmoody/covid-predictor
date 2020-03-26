f75aa4df013a10b05f949afa470c78fac5c59f59
Mahmood Hikmet <mahmood@hikmet.co.nz>
2020-03-26 20:32:12 +1300

    Working for the most part with datahub's dataset

--- covid.py
+++ covid.py
@@ -1,21 +1,40 @@
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import numpy as np
+import pandas as pd
+import os
+from glob import glob
+import datetime
+
+DATADIR = "COVID-19\csse_covid_19_data\csse_covid_19_daily_reports"
 
 def func(x, a, b, c):
     return a * np.exp(-b * x) + c
 
+def func_poly_four(x, a, b, c, d, e):
+    return a * np.power(x, 4) + b * np.power(x, 3) + c * np.power(x, 2) + d * x + e
+
 def func_poly(x, a, b, c, d):
     return a * np.power(x, 3) + b * np.power(x, 2) + c * x + d
 
-xdata = np.linspace(-3, 5, 50)
-y = func_poly(xdata, -0.2, 0.8, 1, 1.2)
-np.random.seed(1729)
-y_noise = 0.5 * np.random.normal(size=xdata.size)
-ydata = y #+ y_noise
+df = pd.read_json('data.min.json')
+df_us = df[df['Country'] == 'US'].tail(14)
+print(df_us)
+df_us['y'] = (df_us['Date'] - df_us['Date'].iloc[0]).dt.days
+
+ydata = df_us['Confirmed'].to_numpy()
+
+xdata = df_us['y'].to_numpy()
+print(xdata)
+print()
+print(df_us['y'])
+
 plt.plot(xdata, ydata, 'b-', label='data')
-
 popt, pcov = curve_fit(func_poly, xdata, ydata)
-plt.plot(xdata, func_poly(xdata, *popt), 'r-', label='fit: a=%5.3f, b=%5.3f, c=%5.3f, d=%5.3f' % tuple(popt))
-
+plt.plot(np.linspace(-6,21), func_poly(np.linspace(-6,21), *popt), 'r-', label='fit: a=%5.3f, b=%5.3f, c=%5.3f, d=%5.3f' % tuple(popt))
+print(df_us)
+print(popt)
+print(f"next value is: {func_poly(df_us.iloc[df_us.shape[0]-1]['y']+1, *popt)}")
+print(f"next value is: {func_poly(df_us.iloc[df_us.shape[0]-1]['y']+2, *popt)}")
+print(f"next value is: {func_poly(df_us.iloc[df_us.shape[0]-1]['y']+3, *popt)}")
 plt.show()
