0d2a4c128333b683bd65f4e0281f2bfac3228981
Mahmood Hikmet <mahmood@hikmet.co.nz>
2020-04-13 20:29:27 +1200

    changed update period to 10 minutes

--- core/updater.py
+++ core/updater.py
@@ -4,6 +4,6 @@ from core.tasks import update_confirmed_stats, update_death_stats
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(update_confirmed_stats, max_instances=1, trigger='cron', minute='*/1')
-    scheduler.add_job(update_death_stats, max_instances=1, trigger='cron', minute='*/1')
+    scheduler.add_job(update_confirmed_stats, max_instances=1, trigger='cron', minute='*/10')
+    scheduler.add_job(update_death_stats, max_instances=1, trigger='cron', minute='*/10')
     scheduler.start()
